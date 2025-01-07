e2c3af3f4900a24ef12ccf6048c6f6350d3f5d4e
ÇİNGENE BARAN <denizbaranaydin2@gmail.com>
2025-01-07 09:24:14 +0300

    Add files via upload

---
+++ 14_10.12.2024/ders/script.js
@@ -0,0 +1,6 @@
+document.getElementById("btn").onclick = function () {
+  console.log("selamın aleyküm");
+  document.getElementById("kutu1").style.backgroundColor = "yellow";
+  document.body.style.backgroundColor = "black";
+  document.getElementById("kutu1").style.borderRadius = "15px";
+};
